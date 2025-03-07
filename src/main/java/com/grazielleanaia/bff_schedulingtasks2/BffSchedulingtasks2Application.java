8e33c788293362ca9d6099d75d8769ef94f1868e
Grazielle <grazielle.anaia@gmail.com>
2025-03-07 09:38:22 -0500

    initial commit

---
+++ src/main/java/com/grazielleanaia/bff_schedulingtasks2/BffSchedulingtasks2Application.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.bff_schedulingtasks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffSchedulingtasks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffSchedulingtasks2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grazielleanaia/bff_schedulingtasks2/BffSchedulingtasks2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.bff_schedulingtasks2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffSchedulingtasks2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
